5f5cb2966a4ec4e4ffaecec9abc60dc1c044fd70
khalduon <khalduon@DESKTOP-IV0INRN>
2023-12-08 05:04:27 -0800

    Add project files.

---
+++ req_login/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace req_login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.lbl_pass = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_name.Location = new System.Drawing.Point(43, 65);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(82, 19);
+            this.lbl_name.TabIndex = 0;
+            this.lbl_name.Text = "user name ";
+            // 
+            // lbl_pass
+            // 
+            this.lbl_pass.AutoSize = true;
+            this.lbl_pass.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_pass.Location = new System.Drawing.Point(43, 128);
+            this.lbl_pass.Name = "lbl_pass";
+            this.lbl_pass.Size = new System.Drawing.Size(73, 19);
+            this.lbl_pass.TabIndex = 1;
+            this.lbl_pass.Text = "password";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(158, 66);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(134, 20);
+            this.txt_name.TabIndex = 2;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(158, 129);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(134, 20);
+            this.txt_pass.TabIndex = 3;
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.BurlyWood;
+            this.btn1.ForeColor = System.Drawing.Color.Black;
+            this.btn1.Location = new System.Drawing.Point(94, 171);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "go";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(371, 250);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.lbl_pass);
+            this.Controls.Add(this.lbl_name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Label lbl_pass;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ req_login/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace req_login
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public object form1 { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void btn1_Click_1(object sender, EventArgs e)
+        {
+            string us = "kawther";
+            string pass = "1234";
+
+
+
+
+            if (txt_name.Text == us && txt_pass.Text == pass)
+            {
+                this.Hide();
+
+                Form2 form2 = new Form2();
+                form2.Show();
+            }
+            else
+            {
+
+                MessageBox.Show("the username or password false");
+            }
+        }
+    }
+    
+}
